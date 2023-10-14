d7f9765b702e44dfd8d499c012ea4a5bb46c8e92
jiya2v <jiya2v@gmail.com>
2023-10-14 17:46:56 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,41 @@
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
+import './index.css';
 
-function App() {
+const App = () => {
+  const [dob, setDob] = useState('');
+  const [age, setAge] = useState('');
+
+  const calculateAge = () => {
+    const birthDate = new Date(dob);
+    const currentDate = new Date();
+
+    const differenceInMilliseconds = currentDate - birthDate;
+    const ageDate = new Date(differenceInMilliseconds);
+    const calculatedAge = Math.abs(ageDate.getUTCFullYear() - 1970);
+
+    setAge(calculatedAge);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+    <div className="container">
+      <center>
+        <h1>Age Calculator</h1>
+        <h3>Enter your date of birth</h3>
+        <input type="date" value={dob} onChange={(e) => setDob(e.target.value)}/>
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          <button
+            onClick={calculateAge}
+            style={{ backgroundColor: '#3498db', color: 'white'}}
+          >
+            Calculate Age
+          </button>
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        {age && <h2 className="result">You are {age} years old</h2>}
+      </center>
     </div>
   );
-}
+};
 
 export default App;
+
